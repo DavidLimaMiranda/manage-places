4a385fc548001d353f0fa8f5e9ca8b64e108f36e
DavidLimaMiranda <oleoloeloel@gmail.com>
2025-02-14 22:47:51 -0300

    Correção na entidade Place, adição dos metodos GET DELETE

--- src/main/java/com/estudo/project/domain/web/PlaceController.java
+++ src/main/java/com/estudo/project/domain/web/PlaceController.java
@@ -2,14 +2,13 @@ package com.estudo.project.domain.web;
 
 import com.estudo.project.domain.dtos.PlaceRequestDTO;
 import com.estudo.project.domain.dtos.PlaceResponseDTO;
+import com.estudo.project.domain.entities.Place;
 import com.estudo.project.domain.services.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,5 +22,18 @@ public class PlaceController {
         var responsePlace = placeService.create(place).map(PlaceMapper::PlaceToResponse);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responsePlace);
+    }
+
+    @GetMapping
+    public ResponseEntity<Flux<Place>> findAllPlaces() {
+
+        var places = placeService.findAllPlaces();
+
+        return ResponseEntity.status(HttpStatus.OK).body(places);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deletePlaceById(@PathVariable Long id) {
+        return placeService.deletePlaceById(id);
     }
 }
